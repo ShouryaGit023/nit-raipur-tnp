bfb559dee23b52bb07df69690f234da9292f3d5c
Shourya <shouryasinha23@gmail.com>
2026-01-10 15:22:03 +0530

    added scrollToTop

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,9 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
+import { useLayoutEffect } from "react";
+
 
 // CRITICAL: Update the extensions of all local imports to use the new .jsx extension
 import Index from "./pages/Index.jsx"; 
@@ -18,6 +20,17 @@ import Developer from "./pages/Developer.jsx";
 
 const queryClient = new QueryClient();
 
+function ScrollToTopOnRouteChange() {
+  const location = useLocation();
+
+  useLayoutEffect(() => {
+    window.scrollTo(0, 0);
+  }, [location.key]);
+
+  return null;
+}
+
+
 // The component itself requires no syntax changes, as it was already pure JSX/JS
 const App = () => (
   <QueryClientProvider client={queryClient}>
@@ -25,6 +38,7 @@ const App = () => (
       <Toaster />
       <Sonner />
       <BrowserRouter>
+      <ScrollToTopOnRouteChange />
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/academics" element={<Academics />} />
